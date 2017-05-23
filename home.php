654fc339bb24aff830bf1effe60d10a28c63639d
Roland Vrignon <roland.vrignon@gmail.com>
2017-05-23 15:12:20 +0200

    Minor css changes

--- home.php
+++ home.php
@@ -12,7 +12,7 @@ exit();}
 <section class="home">
 
         <div class="container-fluid">
-            <h1 class="title text-center">Organisez votre évênement</h1>
+            <h1 class="title text-center">Organiser votre évênement</h1>
             <h2 class="text-center">n'a jamais été aussi simple.</h2>
             <div class="row">
                 <div class="col-sm-6 col-xs-6 col-md-3 text-center">
@@ -31,13 +31,32 @@ exit();}
         </div>
 </section>
 
-<section class="test">
-    <div class="container">
-        <div class="row">
+<section class="template">
+    <div id="picker">
+        <div class="container-fluid">
+            <h1 class="text-center"> Quel type de soirée voules vous ? </h1>
 
+            <div id="genre-component">
+                <div class="sound-kind-picker-component">
+                    <a href=""><li class="kind-picker-select kind-picker">Barbecue</li></a>
+                    <a href=""><li class="kind-picker-select kind-picker">Soirée alcoolisé</li></a>
+                    <a href=""><li class="kind-picker-select kind-picker">Soirée non alcoolisé (BOOM)</li></a>
+                    <a href=""><li class="kind-picker-select kind-picker">Dîner</li></a>
+                    <a href=""><li class="kind-picker-select kind-picker">Soirée étudiante</li></a>
+                    <a href=""><li class="kind-picker-select kind-picker">Anniversaire</li></a>
+                    <a href=""><li class="kind-picker-select kind-picker">Pique-Nique</li></a>
+                    <a href=""><li class="kind-picker-select kind-picker">Soirée en Ville</li></a>
+                    <a href=""><li class="kind-picker-select kind-picker">Anniversaire surprise</li></a>
+                    <a href=""><li class="kind-picker-select kind-picker">Plancha</li></a>
+                    <a href=""><li class="kind-picker-select kind-picker">Soirée à pas beaucoup</li></a>
+                    <a href=""><li class="kind-picker-select kind-picker">Foot</li></a>
+                </div>
+            </div>
         </div>
     </div>
 </section>
+
+
 <?php
 require ('inc/footer.php');
 ?>
--- inc/connectheader.php
+++ inc/connectheader.php
@@ -34,63 +34,18 @@ require ('functions.php');
         </div>
     <?php endforeach; ?>
     <?php unset ($_SESSION['flash'])?>
+
+    <?php
+    if(!isset($_SESSION['auth'])){
+    $_SESSION['flash']['danger'] = "Vous n'avez pas le droit d'accéder à cette page. Veuillez vous connecter d'abord";
+    header('Location: login.php');
+    exit();}?>
+
 <?php endif; ?>
 
-<!--<nav class="navbar navbar-default">-->
-<!--    <div class="container-fluid">-->
-<!--        <!-- Brand and toggle get grouped for better mobile display -->
-<!--        <div class="navbar-header">-->
-<!--            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-<!--                <span class="sr-only">Toggle navigation</span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--            </button>-->
-<!--            <a class="navbar-brand" href="#">Part'it</a>-->
-<!--        </div>-->
-<!-
-<!--        <!-- Collect the nav links, forms, and other content for toggling -->
-<!--        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">-->
-<!--            <ul class="nav navbar-nav">-->
-<!--                <li><a href="home.php">Accueil <span class="sr-only">(current)</span></a></li>-->
-<!--                <li><a href="list_users.php">Liste des utilisateurs</a></li>-->
-<!--                <li class="dropdown">-->
-<!--                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>-->
-<!--                    <ul class="dropdown-menu">-->
-<!--                        <li><a href="#">Action</a></li>-->
-<!--                        <li><a href="#">Another action</a></li>-->
-<!--                        <li><a href="#">Something else here</a></li>-->
-<!--                        <li role="separator" class="divider"></li>-->
-<!--                        <li><a href="#">Separated link</a></li>-->
-<!--                        <li role="separator" class="divider"></li>-->
-<!--                        <li><a href="#">One more separated link</a></li>-->
-<!--                    </ul>-->
-<!--                </li>-->
-<!--            </ul>-->
-<!--            <form class="navbar-form navbar-left">-->
-<!--                <div class="form-group">-->
-<!--                    <input type="text" class="form-control" placeholder="Search">-->
-<!--                </div>-->
-<!--                <button type="submit" class="btn btn-default">Submit</button>-->
-<!--            </form>-->
-<!--            <ul class="nav navbar-nav navbar-right">-->
-<!--                <li style="background-color: #c7e7c8"><a href="creation_event.php">Créez votre évênement!</a></li>-->
-<!--                <li class="dropdown">-->
-<!--                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>-->
-<!--                    <ul class="dropdown-menu">-->
-<!--                        <li><a href="#">Action</a></li>-->
-<!--                        <li><a href="#">Mon compte</a></li>-->
-<!--                        <li><a href="edit_account.php">Edit Compte</a></li>-->
-<!--                        <li role="separator" class="divider"></li>-->
-<!--                        <li><a href="logout.php">Se déconnecter</a></li>-->
-<!--                    </ul>-->
-<!--                </li>-->
-<!--            </ul>-->
-<!--        </div><!-- /.navbar-collapse -->
-<!--    </div><!-- /.container-fluid -->
-<!--</nav>-->
 
-<-- Left section -->
+<-- Left section
+
     <nav id="nav-lateral" class="text-center">
             <div class="img-container text-center dgr-hover-pointer">
                 <img style="height: 100px; margin-top: 10px;" src="img/user.png"><br/>
@@ -111,6 +66,6 @@ require ('functions.php');
 
         </div>
     </nav>
-<-- Right section -->
+<-- Right section
     <div class="container"  style="Position: relatif;" id="cote-droit">
 
